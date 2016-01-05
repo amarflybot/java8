f641577fb9d9492302858933f9038e018aac3618
Amarendra <kumar_amarendra@cat.com>
2016-01-05 07:57:25 +0530

    BinaryTree improvements

--- src/main/java/amar/tree/BinaryTree.java
+++ src/main/java/amar/tree/BinaryTree.java
@@ -1,6 +1,7 @@
 package amar.tree;
 
 /**
+ * Example for BinaryTree in java
  * Created by amarendra on 05/01/16.
  */
 public class BinaryTree {
@@ -36,6 +37,27 @@ public class BinaryTree {
         }
     }
 
+    public static void main(String[] args) {
+
+        BinaryTree binaryTree = new BinaryTree();
+        binaryTree.addNode(50, "Boss50");
+        binaryTree.addNode(45, "Boss45");
+        binaryTree.addNode(40, "Boss40");
+        binaryTree.addNode(58, "Boss58");
+        binaryTree.addNode(59, "Boss59");
+        binaryTree.addNode(23, "Boss23");
+
+        System.out.println("====================inOrderTraverseTree======================");
+        binaryTree.inOrderTraverseTree(binaryTree.root);
+        System.out.println("====================preOrderTraverseTree======================");
+        binaryTree.preOrderTraverseTree(binaryTree.root);
+        System.out.println("====================postOrderTraverseTree======================");
+        binaryTree.postOrderTraverseTree(binaryTree.root);
+        System.out.println("====================findNode======================");
+        System.out.println(binaryTree.findNode(599));
+
+    }
+
     public void inOrderTraverseTree(Node focusNode){
 
         if(focusNode != null){
@@ -47,17 +69,40 @@ public class BinaryTree {
         }
     }
 
-    public static void main(String[] args) {
 
-        BinaryTree binaryTree = new BinaryTree();
-        binaryTree.addNode(50, "Boss50");
-        binaryTree.addNode(45, "Boss45");
-        binaryTree.addNode(40, "Boss40");
-        binaryTree.addNode(58, "Boss58");
-        binaryTree.addNode(59, "Boss59");
-        binaryTree.addNode(23, "Boss23");
-        binaryTree.inOrderTraverseTree(binaryTree.root);
 
+    public void preOrderTraverseTree(Node focusNode) {
+        if(focusNode != null){
+            System.out.println(focusNode);
+            preOrderTraverseTree(focusNode.left);
+            preOrderTraverseTree(focusNode.right);
+        }
+    }
+
+    public void postOrderTraverseTree(Node focusNode) {
+        if(focusNode != null){
+            postOrderTraverseTree(focusNode.left);
+            postOrderTraverseTree(focusNode.right);
+            System.out.println(focusNode);
+        }
+    }
+
+    public Node findNode(int key){
+        Node focusNode = root;
+
+        while (focusNode.key != key){
+
+            if(key < focusNode.key){
+                focusNode = focusNode.left;
+            }
+            else {
+                focusNode = focusNode.right;
+            }
+            if(focusNode == null){
+                return null;
+            }
+        }
+        return focusNode;
     }
 }
 
