8161cc4ffd7f7cc57f4bd3343e081cf959b49518
amarraja123 <amar.arawat@gmail.com>
2016-01-06 23:57:35 +0530

    DataStructure

---
+++ src/main/java/amar/ds/StackAsQueueTest.java
@@ -0,0 +1,52 @@
+package amar.ds;
+
+import java.util.Stack;
+
+/**
+ * Created by amarendra on 06/01/16.
+ */
+public class StackAsQueueTest {
+
+    public static void main(String[] args) {
+
+        StackAsQueue<Integer> stackAsQueue = new StackAsQueue();
+        stackAsQueue.push(1);
+        stackAsQueue.push(2);
+        stackAsQueue.push(3);
+        stackAsQueue.push(4);
+        stackAsQueue.push(5);
+        stackAsQueue.push(6);
+
+        System.out.println(stackAsQueue);
+
+        stackAsQueue.pop();
+        stackAsQueue.pop();
+
+        System.out.println(stackAsQueue);
+
+    }
+
+}
+
+class StackAsQueue<T> {
+
+    private Stack<T> stack1 = new Stack<>();
+
+    public void push(T t){
+        stack1.push(t);
+    }
+
+    public T pop(){
+        T result = null;
+        if (stack1.size()>0) {
+            result = stack1.get(0);
+            stack1.remove(0);
+        }
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return stack1.toString();
+    }
+}
