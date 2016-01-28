95e15a909f5a0539bf1e622545448d7c065f0cf7
amarraja123 <amar.arawat@gmail.com>
2016-01-28 10:31:35 +0530

    immutable

--- src/main/java/amar/designPattern/immutable/ImmutableTest.java
+++ src/main/java/amar/designPattern/immutable/ImmutableTest.java
@@ -11,14 +11,25 @@ public class ImmutableTest {
 
     public static void main(String[] args) {
         Roll roll = new Roll(1);
-        Person person = new Person(roll, new Date());
-        System.out.println(person.getDate());
+        Roll roll12 = new Roll(12);
+        Roll roll13 = new Roll(13);
+        Roll roll14 = new Roll(14);
+        Roll roll15 = new Roll(15);
+        ArrayList<Roll> rollList = new ArrayList<>();
+        rollList.add(roll);
+        rollList.add(roll12);
+        rollList.add(roll13);
+        rollList.add(roll14);
+        rollList.add(roll15);
+        Date date = new Date();
+        Person person = new Person(roll, date, rollList);
         System.out.println(person);
         roll.setInteger(2);
         person.getRoll().setInteger(2);
         person.getDate().setTime(new Date().getTime());
+        person.getRollList().add(new Roll(21));
         System.out.println(person);
-        System.out.println(person.getDate());
+
     }
 }
 
@@ -50,6 +61,7 @@ final class Person{
         return "Person{" +
                 "roll=" + roll +
                 ", date=" + date +
+                ", rollList=" + rollList +
                 '}';
     }
 }
