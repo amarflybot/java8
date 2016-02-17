93248fc26bc944b878bc1b52d16e2e952aa4180a
amarraja123 <amar.arawat@gmail.com>
2016-02-17 23:02:05 +0530

    Fibonacci

--- src/main/java/amar/ds/FibonacciTest.java
+++ src/main/java/amar/ds/FibonacciTest.java
@@ -1,6 +1,6 @@
 package amar.ds;
 
-import java.util.Set;
+import java.util.Arrays;
 import java.util.Stack;
 
 /**
@@ -9,20 +9,21 @@ import java.util.Stack;
 public class FibonacciTest {
 
     public static void main(String[] args) {
-        Stack<Integer> integerStack = new Stack<>();
-        integerStack.push(0);
-        integerStack.push(1);
-        Stack<Integer> result = findFibonacci(integerStack, 8);
-        System.out.println(result);
+        int [] integerStack = new int [7];
+        integerStack[0] = 0;
+        integerStack[1] = 1;
+        int[] fibonacci = findFibonacci(integerStack, 8, 1);
+        System.out.println(Arrays.toString(fibonacci));
     }
 
-    private static Stack<Integer> findFibonacci(Stack<Integer> result, int i) {
+    private static int[] findFibonacci(int[] result, int i, int j) {
 
-        if(result.peek() >= i){
+        if(result[j] >= i){
             return null;
         }
-        result.push(result.get(result.size()-2)+result.peek());
-        findFibonacci(result, i);
+        result[j+1] = (result[j]+result[j-1]);
+        j = j+1;
+        findFibonacci(result, i, j);
         return result;
     }
 
