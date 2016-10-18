312b17f24596a19709605dde5caf37dbe3a801b5
Amarendra Kumar <amar.arawat@gmail.com>
2016-10-18 10:36:15 +0530

    Change for RX java TimeBased Ticker

--- src/main/java/amar/rx/filters/TimeBasedExampleSample.java
+++ src/main/java/amar/rx/filters/TimeBasedExampleSample.java
@@ -1,5 +1,8 @@
 package amar.rx.filters;
 
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by Amarendra Kumar on 10/18/2016.
  */
@@ -7,7 +10,25 @@ public class TimeBasedExampleSample {
 
     public static void main(String[] args) {
 
-        TimeTicker timeTicker = new TimeTicker();
+        TimeTicker timeTicker = null;
+        try {
+            timeTicker = new TimeTicker(10);
+            timeTicker.start();
+
+            timeTicker.toObservable()
+                    .sample(1, TimeUnit.SECONDS)
+                    .subscribe((t) -> {
+                        System.out.println("Tick: " + new Date(t));
+                    });
+
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            //Do nothing
+        } finally {
+            timeTicker.stop();
+        }
+
+        System.exit(0);
 
     }
 }
--- src/main/java/amar/rx/filters/TimeTicker.java
+++ src/main/java/amar/rx/filters/TimeTicker.java
@@ -1,10 +1,83 @@
 package amar.rx.filters;
 
+import rx.Observable;
+import rx.subjects.BehaviorSubject;
+
 /**
  * Created by Amarendra Kumar on 10/18/2016.
  */
 public class TimeTicker {
 
-    private final
+    private final BehaviorSubject<Long> tickerSubject;
+    private final long interval;
 
+    private volatile boolean paused;
+    private long lastTick;
+    private Thread tickerThread;
+
+    public TimeTicker(long interval) {
+        this.lastTick = System.currentTimeMillis();
+        this.tickerSubject = BehaviorSubject.create(lastTick);
+        this.tickerThread = null;
+        this.paused = false;
+        this.interval = interval;
+    }
+
+    public Observable<Long> toObservable(){
+        return tickerSubject;
+    }
+
+    public synchronized  void start(){
+
+        if (tickerThread != null){
+            return; // the ticker thread is already running;
+        }
+
+        //make sure the pause flag is false
+        unpause();
+
+        tickerThread = new Thread(()->{
+            try {
+                while (Thread.interrupted() == false){
+
+                    try {
+                        Thread.sleep(5);
+                    } catch (InterruptedException e) {
+                        //e.printStackTrace();
+                        break;
+                    }
+                    if(paused){
+                        continue;
+                    }
+
+                    long currentTime = System.currentTimeMillis();
+                    if (currentTime - lastTick > interval)
+                    lastTick = currentTime;
+                    tickerSubject.onNext(lastTick);
+                }
+            } catch (Exception e) {
+                tickerSubject.onError(e);
+            }
+
+            tickerSubject.onCompleted();
+        }, "TickerThread");
+        tickerThread.start();
+    }
+
+    public synchronized void stop(){
+        if (tickerThread == null){
+            return;
+        }
+        tickerThread.interrupt();
+
+        try {
+            tickerThread.join();
+        } catch (InterruptedException e) {
+            //e.printStackTrace();
+        }
+    }
+
+    private void unpause() {
+        this.paused = false;
+    }
 }
