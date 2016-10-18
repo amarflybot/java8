f1a57e2f9d1fe7c799ce7ee0523963b222fb2888
Amarendra Kumar <amar.arawat@gmail.com>
2016-10-18 18:17:00 +0530

    Change for RX java Timed Conditional

--- src/main/java/amar/rx/transformer/ConditionalSkipExample.java
+++ src/main/java/amar/rx/transformer/ConditionalSkipExample.java
@@ -4,6 +4,7 @@ import amar.rx.filters.TimeTicker;
 import amar.rx.intro.DataGenerator;
 import rx.Observable;
 import rx.functions.Func1;
+import rx.schedulers.TimeInterval;
 
 import java.util.concurrent.TimeUnit;
 
@@ -15,21 +16,28 @@ public class ConditionalSkipExample {
     public static void main(String[] args) throws InterruptedException {
 
 
-        Observable<String> sequence1 = Observable.interval(50, TimeUnit.MILLISECONDS)
-                .repeat()
-                .flatMap((Func1<Long, Observable<String>>) aLong -> Observable.from(DataGenerator.generateGreekAlphabet()));
-        TimeTicker ticker = new TimeTicker(30000);
+        Observable<String> sequence1 = Observable.interval(50l, TimeUnit.MILLISECONDS)
+                .timeInterval()
+                .flatMap(new Func1<TimeInterval<Long>, Observable<String>>() {
+                    @Override
+                    public Observable<String> call(TimeInterval<Long> longTimeInterval) {
+                        return Observable.from(DataGenerator.generateGreekAlphabet());
+                    }
+                });
+        TimeTicker ticker = new TimeTicker(3000l);
+
+        ticker.start();
 
         sequence1.asObservable()
                 .skipUntil(ticker.toObservable())
+                //.takeUntil(ticker.toObservable())
                 .subscribe(ch -> {
                     System.out.println(ch);
                 });
 
-        ticker.start();
-        sequence1.startWith("Amar");
+        //sequence1.startWith("Amar");
 
-        Thread.sleep(60000);
+        Thread.sleep(6000l);
 
         System.exit(0);
     }
