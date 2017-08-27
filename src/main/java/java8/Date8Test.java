15e937d31295fa97997d8f636f80e727ac76585f
amarraja123 <amar.arawat@gmail.com>
2017-08-27 17:58:41 -0400

    Date in java8

---
+++ src/main/java/java8/Date8Test.java
@@ -0,0 +1,25 @@
+package java8;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+/**
+ * Created by amarendra on 27/08/17.
+ */
+public class Date8Test {
+
+    public static void main(String[] args) {
+
+        System.out.println(Instant.now());
+        System.out.println(LocalDate.now());
+        System.out.println(LocalDate.now(ZoneId.of(ZoneId.SHORT_IDS.get("IST"))));
+        LocalDate of = LocalDate.of(1564, Month.APRIL, 30);
+        String format = DateTimeFormatter.ISO_DATE_TIME.format(LocalDateTime.now());
+        System.out.println("formatted -> "+ format);
+        System.out.println(of);
+        Period until = of.until(LocalDate.now());
+        System.out.println(until.get(ChronoUnit.DAYS));
+
+    }
+}
