c2417498dcd0bb59277932bbb90b0d9d136b20dc
amarraja123 <amar.arawat@gmail.com>
2017-09-17 18:16:04 -0400

    Learning Thread

---
+++ src/main/java/amar/thread/AccountTransfer.java
@@ -0,0 +1,88 @@
+package amar.thread;
+
+import java.util.Random;
+
+/**
+ * Created by amarendra on 17/09/17.
+ */
+public class AccountTransfer {
+    public static void main(String[] args) throws InterruptedException {
+
+        AccountRunner accountRunner = new AccountRunner();
+
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                accountRunner.firstThread();
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                accountRunner.secondThread();
+            }
+        });
+
+        t1.start();t2.start();
+        t1.join();t2.join();
+
+        accountRunner.finish();
+    }
+
+}
+
+class AccountRunner{
+
+    private Account account1 = new Account();
+    private Account account2 = new Account();
+
+    public void firstThread(){
+        Random random = new Random();
+        for (int i = 0; i < 10000; i++) {
+            Account.transfer(account1,account2,random.nextInt(100));
+        }
+    }
+
+    public void secondThread(){
+        Random random = new Random();
+        for (int i = 0; i < 10000; i++) {
+            Account.transfer(account2,account1,random.nextInt(100));
+        }
+    }
+
+    public void finish(){
+        System.out.println("Account1 : "+ account1);
+        System.out.println("Account2 : "+ account2);
+        System.out.println("Total balance: "+ (account1.getBalance()+account2.getBalance()));
+    }
+
+}
+
+class Account {
+    private int balance = 10000;
+
+    public void deposit(int amount){
+        balance = balance + amount;
+    }
+
+    public void withdraw(int amount){
+        balance = balance - amount;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public static void transfer(Account account1, Account account2, int amount){
+        account1.withdraw(amount);
+        account2.deposit(amount);
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "balance=" + balance +
+                '}';
+    }
+}
---
+++ src/main/java/amar/thread/CounterSafety.java
@@ -0,0 +1,97 @@
+package amar.thread;
+
+import java.util.Scanner;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by amarendra on 17/09/17.
+ */
+public class CounterSafety {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        for (int i = 0; i < 10; i++) {
+
+            getItRunning();
+        }
+    }
+
+    public static void getItRunning() throws InterruptedException {
+        Runner runner = new Runner();
+
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    runner.firstThread();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    runner.secondThread();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+
+        runner.finish();
+    }
+}
+
+class Runner
+{
+    private Lock lock = new ReentrantLock();
+    private Condition condition = lock.newCondition();
+    private int count;
+
+    private void increment(){
+        for (int i = 0; i < 10000; i++) {
+            count++;
+        }
+    }
+
+    public void firstThread() throws InterruptedException {
+        lock.lock();
+        System.out.println("Waiting");
+        condition.await();
+        System.out.println("Woken up");
+        try {
+            increment();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void secondThread() throws InterruptedException {
+        lock.lock();
+        Thread.sleep(1000);
+        System.out.println("Process Return key");
+        new Scanner(System.in).nextLine();
+        System.out.println("Key Pressed");
+        condition.signal();
+        try {
+            increment();
+        } finally {
+
+            lock.unlock();
+        }
+    }
+
+    public void finish() {
+        System.out.println("After all done, count = "+ count);
+    }
+}
